90c1f3a6687f1c96e66dd9687063059ad16fe374
jonghunbaek <qor6028@naver.com>
2023-06-06 22:08:46 +0900

    first commit

---
+++ own-your-fit/src/main/java/com/ownyourfit/ownyourfit/OwnYourFitApplication.java
@@ -0,0 +1,13 @@
+package com.ownyourfit.ownyourfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnYourFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnYourFitApplication.class, args);
+	}
+
+}
---
+++ own-your-fit/src/test/java/com/ownyourfit/ownyourfit/OwnYourFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownyourfit.ownyourfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnYourFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
