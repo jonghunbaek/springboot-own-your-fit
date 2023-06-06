ae6f4e4540bc62bfe0dec852b1cdfd99e58061f4
jonghunbaek <qor6028@naver.com>
2023-06-06 22:10:19 +0900

    docs : 패키지명 수정

--- own-your-fit/src/main/java/com/ownyourfit/OwnYourFitApplication.java
+++ own-your-fit/src/main/java/com/ownyourfit/OwnYourFitApplication.java
@@ -1,4 +1,4 @@
-package com.ownyourfit.ownyourfit;
+package com.ownyourfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- own-your-fit/src/test/java/com/ownyourfit/OwnYourFitApplicationTests.java
+++ own-your-fit/src/test/java/com/ownyourfit/OwnYourFitApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ownyourfit.ownyourfit;
+package com.ownyourfit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
