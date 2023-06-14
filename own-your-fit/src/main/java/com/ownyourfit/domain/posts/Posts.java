34c38f6d468aac472567cb995bb48e11c47d3aa4
jonghunbaek <qor6028@naver.com>
2023-06-14 21:45:05 +0900

    fix : JPA테스트 코드 오류 수정

--- own-your-fit/src/main/java/com/ownyourfit/domain/posts/Posts.java
+++ own-your-fit/src/main/java/com/ownyourfit/domain/posts/Posts.java
@@ -21,7 +21,7 @@ public class Posts {
 	@Column(length = 500, nullable = false)
 	private String title;
 	
-	@Column(columnDefinition = "TEXT", nullable = false)
+	@Column(length = 1000, nullable = false)
 	private String content;
 	
 	private String author;
